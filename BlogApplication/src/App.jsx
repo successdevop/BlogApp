1277489864e2115637f6a1c1861d023a38b40501
successdevop <successraphael28@gmail.com>
2023-11-15 00:22:53 +0100

    partially completed navbar

--- BlogApplication/src/App.jsx
+++ BlogApplication/src/App.jsx
@@ -1,8 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import {
+  ABOUT,
+  BLOG,
+  CONTACT,
+  HOME,
+  SUBSCRIBE,
+} from "./assets/constants/routePaths";
+import HomePage from "./pages/HomePage";
+import BlogPage from "./pages/BlogPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+
 function App() {
   return (
-    <h1 className="text-xl font-bold text-red-500 text-center mt-[2rem]">
-      Hello tailwind css
-    </h1>
+    <Routes>
+      <Route path={HOME} element={<HomePage />} />
+      <Route path={BLOG} element={<BlogPage />} />
+      <Route path={ABOUT} element={<AboutPage />} />
+      <Route path={CONTACT} element={<ContactPage />} />
+      <Route path={SUBSCRIBE} element={<HomePage />} />
+    </Routes>
   );
 }
 
---
+++ BlogApplication/src/assets/constants/routePaths.js
@@ -0,0 +1,5 @@
+export const HOME = "/";
+export const BLOG = "/blog";
+export const ABOUT = "/about";
+export const CONTACT = "/contact";
+export const SUBSCRIBE = "/subscribe";
